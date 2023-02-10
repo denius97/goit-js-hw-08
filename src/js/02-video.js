2151b9ff954538a27331947ae8c609eb10da535c
denius97 <106709563+denius97@users.noreply.github.com>
2023-02-10 17:14:14 +0200

    fix some

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,11 @@ const throttle = require('lodash.throttle');
 
 const player = new Player('vimeo-player');
 
-player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+player
+  .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+  .catch(function (error) {
+    console.log('Error set time', error);
+  });
 
 player.on(
   'timeupdate',
